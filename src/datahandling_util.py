031b4708f4e5e6b266ab8be4f156d27e9c96d72a
StevenDankMeister <stran19@student.aau.dk>
2023-03-01 10:17:56 +0100

    split_data function

--- src/datahandling_util.py
+++ src/datahandling_util.py
@@ -1,6 +1,7 @@
 import torch
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+from torch.utils.data import ConcatDataset, Subset
 
 def get_data(dir="./data"):
     train_data = datasets.MNIST(
@@ -25,3 +26,29 @@ def load_data(train_data, test_data, batch_size=100):
     }
 
     return loaders
+
+    
+# Remove classes from train and test data and add them to support data
+def split_data(train_data, test_data, targets):
+    idx_to_remove = [i for i, x in enumerate(train_data.targets) if x in targets]
+    idx_to_remove_test = [i for i, x in enumerate(test_data.targets) if x in targets]
+
+    idx_to_keep = [i for i, x in enumerate(train_data.targets) if x not in targets]
+    idx_to_keep_test = [i for i, x in enumerate(test_data.targets) if x not in targets]
+
+    removed = Subset(train_data.data, idx_to_remove)
+    removed.targets = Subset(train_data.targets, idx_to_remove)
+
+    test_removed = Subset(test_data.data, idx_to_remove_test)
+    test_removed.targets = Subset(test_data.targets, idx_to_remove_test)
+
+    support_data = ConcatDataset([removed, test_removed])
+    
+    train_data.data = Subset(train_data.data, idx_to_keep)
+    train_data.targets = Subset(train_data.targets, idx_to_keep)
+
+    test_data.data = Subset(test_data.data, idx_to_keep_test)
+    test_data.targets = Subset(test_data.targets, idx_to_keep_test)
+
+    return train_data, test_data, support_data
+
