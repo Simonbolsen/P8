5ecc7241d182a9df6228a0689abd54fa2e822a62
StevenDankMeister <stran19@student.aau.dk>
2023-03-01 12:30:51 +0100

    Fixed splitdata and kshotloader (didnt work before

--- src/datahandling_util.py
+++ src/datahandling_util.py
@@ -1,7 +1,18 @@
 import torch
 from torchvision import datasets
 from torchvision.transforms import ToTensor
-from torch.utils.data import ConcatDataset, Subset
+from torch.utils.data import ConcatDataset, Subset, TensorDataset
+
+class SupportDataset(torch.utils.data.Dataset):
+    def __init__(self, support_data, support_data_targets):
+        self.data = support_data
+        self.targets = support_data_targets
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.targets[idx]
 
 def get_data(dir="./data"):
     train_data = datasets.MNIST(
@@ -36,32 +47,34 @@ def split_data(train_data, test_data, targets):
     idx_to_keep = [i for i, x in enumerate(train_data.targets) if x not in targets]
     idx_to_keep_test = [i for i, x in enumerate(test_data.targets) if x not in targets]
 
-    removed = Subset(train_data.data, idx_to_remove)
-    removed.targets = Subset(train_data.targets, idx_to_remove)
-
-    test_removed = Subset(test_data.data, idx_to_remove_test)
-    test_removed.targets = Subset(test_data.targets, idx_to_remove_test)
-
-    support_data = ConcatDataset([removed, test_removed])
+    support_data_concat = torch.cat((train_data.data[idx_to_remove], 
+                                     test_data.data[idx_to_remove_test]), 0)
     
-    train_data.data = Subset(train_data.data, idx_to_keep)
-    train_data.targets = Subset(train_data.targets, idx_to_keep)
+    support_targets_concat = torch.cat((train_data.targets[idx_to_remove], 
+                                        test_data.targets[idx_to_remove_test]), 0)
+    
+    support_dataset = SupportDataset(support_data_concat, support_targets_concat)
+    
+    train_data.data = train_data.data[idx_to_keep]
+    train_data.targets = train_data.targets[idx_to_keep]
 
-    test_data.data = Subset(test_data.data, idx_to_keep_test)
-    test_data.targets = Subset(test_data.targets, idx_to_keep_test)
+    test_data.data = test_data.data[idx_to_keep_test]
+    test_data.targets = test_data.targets[idx_to_keep_test]
 
-    return train_data, test_data, support_data
+    return train_data, test_data, support_dataset
 
 # Create k loaders for each class in support data 
 # with batch size of shots
 def k_shot_loaders(support_data, k, shots):
     loaders = []
-    targets = set(support_data.targets)
+    targets = torch.unique(support_data.targets)
+    
+    assert len(targets) == k
 
     for i in range(k):
         target = targets[i]
         idx_targets = [j for j, x in enumerate(support_data.targets) if x == target]
-        subset_data = Subset(support_data.data, idx_targets)
+        subset_data = Subset(support_data, idx_targets)
         loaders.append(torch.utils.data.DataLoader(subset_data, batch_size=shots, shuffle=True, num_workers=1))
 
     return loaders
