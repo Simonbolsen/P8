863d9432b2c54ad0644f6c0f04a46246b43127fd
Simonbolsen <Simonbolsen1234@gmail.com>
2023-03-03 21:18:31 +0100

    scatter plots

--- src/plotting_util.py
+++ src/plotting_util.py
@@ -43,3 +43,35 @@ def plotSurface(heights, zTitle, xAxis, xTitle, yAxis, yTitle, num_of_surfaces, 
     axe.legend()
 
     plt.show()
+
+
+def plotPoints(xs, ys, zs, axis_names, num_of_series = 1, series_labels=[]):
+    mpl.rcParams['legend.fontsize'] = 10
+
+    if series_labels == []: 
+        series_labels = [axis_names[2] for _ in range(num_of_series)]
+
+    if(not isinstance(xs[0], list)):
+        xs = [xs]
+        ys = [ys]
+        zs = [zs]
+
+    COLOR = cm.rainbow(np.linspace(0, 1, num_of_series))
+    fig = plt.figure()
+    axe = plt.axes(projection='3d')
+
+    for series in range(num_of_series):
+        axe.plot(xs[series], ys[series], zs[series], "o", color=COLOR[series], label=series_labels[series])
+
+    axe.set_xlabel(axis_names[0])
+    axe.set_ylabel(axis_names[1])
+    axe.set_zlabel(axis_names[2])
+
+    axe.legend()
+
+    plt.show()
+
+if __name__ == '__main__':
+    plotPoints([[0, 1, 2, 3]], [[2,2,5,2]], [[3,2,3,4]], ["Length", "Width", "Height"])
+    plotPoints([0, 1, 2, 3], [2,2,5,2], [3,2,3,4], ["Length", "Width", "Height"]) #simpler version for a single series
+    plotPoints([[0, 1], [2, 3]], [[2,2],[5,2]], [[3,2],[3,4]], ["Length", "Width", "Height"], num_of_series=2) #two series, will have different colors
