0baf138de709df6117ff6dd06705ff00cebe31a4
StevenDankMeister <stran19@student.aau.dk>
2023-03-21 15:14:14 +0100

    eval is faster...

--- src/few_shot_utils.py
+++ src/few_shot_utils.py
@@ -13,12 +13,15 @@ import json
 
 def get_few_shot_loaders(config, train_data, few_shot_data):
     train_loader = get_data_loader(train_data, config["batch_size"])
-    fs_sup_loader, fs_query_load = k_shot_loaders(few_shot_data, config["shots"])
+    fs_sup_loaders, fs_query_loader = k_shot_loaders(few_shot_data, config["shots"])
     
-    return train_loader, fs_sup_loader, fs_query_load
+    return train_loader, fs_sup_loaders, fs_query_loader
 
 def setup_few_shot_pretrained(config, model_name, train_data, few_shot_data, device, args, ray_tune = True):
-    train_loader, fs_sup_loader, fs_query_loaders = get_few_shot_loaders(config, ray.get(train_data), ray.get(few_shot_data))
+    train_loader, fs_sup_loaders, fs_query_loader = get_few_shot_loaders(config, ray.get(train_data), ray.get(few_shot_data))
+    print(f"support loaders: {len(fs_sup_loaders)}")
+    print(f"few shot support loader batches: {len(fs_sup_loaders[0])}")
+    print(f"few shot querry loader batches: {len(fs_query_loader)}")
     loss_func = get_loss_function(args)
     num_of_classes = len(train_loader.unique_targets)
     model, _ = load_pretrained(model_name, num_of_classes, 
@@ -26,11 +29,14 @@ def setup_few_shot_pretrained(config, model_name, train_data, few_shot_data, dev
                             train_loader.channels, device)
     model.to(device)
     
-    train_few_shot(config, train_loader, fs_sup_loader, fs_query_loaders, 
+    train_few_shot(config, train_loader, fs_sup_loaders, fs_query_loader, 
                 model, loss_func, device, ray_tune)
 
 def train_few_shot(config, train_loader, fs_sup_loaders, fs_query_load, 
                    model, loss_func, device, ray_tune):
+    
+    print("extracting support images...")
+    support_images = extract_support_images(fs_sup_loaders)
     
     optimiser = optim.Adam(model.parameters(), lr=config["lr"])
     
@@ -40,8 +46,7 @@ def train_few_shot(config, train_loader, fs_sup_loaders, fs_query_load,
     for epoch in range(max_epochs):
         print("training...")
         train(model, train_loader, optimiser, loss_func, max_epochs, epoch, device)
-        print("evaluating...")
-        last_acc = few_shot_eval(model, fs_sup_loaders, fs_query_load, device)
+        last_acc = few_shot_eval(model, fs_sup_loaders, fs_query_load, support_images, device)
         if ray_tune:
             tune.report(accuracy = last_acc)
         else:
@@ -49,19 +54,32 @@ def train_few_shot(config, train_loader, fs_sup_loaders, fs_query_load,
     
     save_few_shot_embedding_result(train_loader, fs_sup_loaders, fs_query_load, model, config, last_acc, device)
 
-def few_shot_eval(model, support_loaders, query_loader, device):
+def extract_support_images(fs_sup_loaders):
+    batches = []
+    for loader in fs_sup_loaders:
+        images, _ = next(iter(loader))
+        batches.append(images)
+        
+    return batches
+
+def few_shot_eval(model, support_loaders, query_loader, support_images, device):
     # Test the model
     model.eval()
     with torch.no_grad():
+        print("evaluating")
         # Get the targets we have not seen before
+        print("calling find few shot targets")
         few_shot_targets = find_few_shot_targets(support_loaders)
+        print("done find few shot targets")
+        print(few_shot_targets)
         num_of_new_classes = len(few_shot_targets)
 
         new_class_embeddings = []
         correct = [0] * num_of_new_classes
         total = [0] * num_of_new_classes
 
-        new_class_embeddings = get_few_shot_embeddings(support_loaders, model, device)
+        print("calculating new embeddings...")
+        new_class_embeddings = get_few_shot_embeddings(support_images, model, device)
         
         # average embeddings for class
         new_class_embeddings = [sum(item) / len(item) for item in new_class_embeddings]
@@ -71,6 +89,7 @@ def few_shot_eval(model, support_loaders, query_loader, device):
         assert len(new_class_embeddings) == len(few_shot_targets)
 
         # do evaluation
+        print("evaluating predictions...")
         for images, labels in query_loader:
             img_size = query_loader.image_size
             channels = query_loader.channels
@@ -78,6 +97,8 @@ def few_shot_eval(model, support_loaders, query_loader, device):
             
             test_output = model(images)
 
+            # find_closest_embeddings(test_output, new_class_embeddings)
+            
             for i, output_embedding in enumerate(test_output[:-model.num_of_classes]):
                 closest_target_index = find_closest_embedding(output_embedding, new_class_embeddings)
                 predicted_target = few_shot_targets[closest_target_index]
@@ -91,21 +112,23 @@ def few_shot_eval(model, support_loaders, query_loader, device):
 def find_few_shot_targets(support_loaders):
     few_shot_targets = []
     for loader in support_loaders:
-        for _, labels in loader:
-            few_shot_targets.append(labels[0].item())
-            break
+        few_shot_targets.extend(loader.unique_targets)
     return few_shot_targets
 
-def get_few_shot_embeddings(support_loaders, model, device):
+def get_few_shot_embeddings(support_images, model, device):
     new_class_embeddings = []
-    for loader in support_loaders:
-        img_size = loader.image_size
-        channels = loader.channels
-       
-        for images, _ in loader:
-            images = images.view(-1, channels, img_size, img_size).float().to(device)
-            few_shot_output = model(images)
-            new_class_embeddings.append(few_shot_output[:-model.num_of_classes])
+    
+    image_size = support_images[0].size()[2]
+    channels = support_images[0].size()[1]
+    
+    print("image_size: ", image_size)
+    print("channels ", channels)
+        
+    for support_batch in support_images:
+        images = support_batch.view(-1, channels, image_size, image_size).float().to(device)
+        few_shot_output = model(images)
+
+        new_class_embeddings.append(few_shot_output[:-model.num_of_classes])
     
     return new_class_embeddings
 
