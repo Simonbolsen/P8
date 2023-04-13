180c7006d6bde5b213c23a3c986b7cb752b42cbb
Simon <Simonbolsen1234@gmail.com>
2023-04-13 14:49:40 +0200

    visualizing embeddings

--- src/embeddingAnalysis/embedding_visualization.py
+++ src/embeddingAnalysis/embedding_visualization.py
@@ -8,11 +8,17 @@ import Plotting.plotting_util as plot
 import numpy as np
 import matplotlib.cm as cm
 import math
+import pickle
 
 def read_json_file(path_to_json_file):
     with open(path_to_json_file, 'r') as json_file:
         data = json_file.read()
     return json.loads(json.loads(data))
+
+def read_pickle_file(path):
+    with open(path, 'rb') as file:
+        data = pickle.load(file)
+        return data
 
 def argmin(iterable):
     return min(enumerate(iterable), key=lambda x: x[1])[0]
@@ -68,30 +74,15 @@ def print_distance_matrix(embeddings):
             print(f"{np.linalg.norm(np.array(i) - np.array(ii)):.2f}",end=" ")
         print("")
 
-def print_distance_matrix(embeddings):
-    center = np.zeros(len(embeddings[0]))
-    print("")
-    for i, embedding in enumerate(embeddings):
-        center = center + np.array(embedding)
-        print(f"  {i}", end="  ")
-
-    center = center / len(embeddings)
-    print("")
-    for index, i in enumerate(embeddings):
-        print(f"{index}", end=" ")
-        for ii in embeddings:
-            print(f"{np.linalg.norm(np.array(i) - np.array(ii)):.2f}",end=" ")
-        print("")
-
-def print_distances_from_center():
+def print_distances_from_center(embeddings):
     
 
-    center = np.zeros(len(class_embeddings[0]))
-    for i in class_embeddings:
+    center = np.zeros(len(embeddings[0]))
+    for i in embeddings:
         center += i
-    center = center / 10
+    center = center / len(embeddings)
     print()
-    for i in class_embeddings:
+    for i in embeddings:
         print(np.linalg.norm(center - i))
 
 def plot_series():
@@ -119,11 +110,11 @@ def plot_series():
     #plot.plotCustomPoints(series, legend=False, axes=[1,2,3])
     #plot.plotCustomPoints(series, legend=False, axes=[0,2,3])
 
-def get_dists(embeddings, labels, class_embeddings):
+def get_dists(embeddings, labels, class_embeddings, func = lambda x: np.log(x)):
     dists = []
     for i, embedding in enumerate(embeddings):
         class_embedding = class_embeddings[labels[i]]
-        dists.append(np.log(np.linalg.norm(np.array(class_embedding) - np.array(embedding))))
+        dists.append(func(np.linalg.norm(np.array(class_embedding) - np.array(embedding))))
     return dists
 
 def get_buckets(dists, maximum, minimum, num_buckets):
@@ -206,31 +197,202 @@ def plot_center_class_projection():
     #plot.plotHeatMap(projections, anti_projections, 50, 50, "Amount")
     plot.plot_points_series_2d(xs, ys)
 
-if __name__ == '__main__':
-    data = read_json_file(os.path.dirname(__file__) + "/../../embeddingData/json_data.json")
-
+def pca_pure_classification():
+    i = 29
+    data = read_pickle_file(os.path.dirname(__file__) + f"/../embeddingData/classification_data_train_{i}.json")
+    #print(f"{i}: {data['accuracy']}")
     train_embeddings = data["train_embeddings"]
-    test_embeddings  = data["test_embeddings"]
-    class_embeddings = data["class_embeddings"]
-    test_labels = data["test_labels"]
+
+    points = 100
+
+    f = lambda x : int(((x + 1)/points)**2 * (1725-points) + x + 1)
+
+    scores = []
+    for i in range(points):
+        pca = PCA(n_components = f(i))
+        _ = pca.fit_transform(train_embeddings)
+        scores.append(pca.score(train_embeddings))
+        if i % int(points / 10) == 0:
+            print(i)
+
+    plot.plot_line_2d([f(i) for i in range(points)], 
+                      [scores], 
+                      ["PCA Scores"], lambda x:x)
+    
+def get_class_centers(embeddings, labels, num_of_classes):
+    class_center_train = [np.zeros(len(embeddings[0])) for _ in range (num_of_classes)]
+    class_size = [0 for _ in range(10)]
+    for i, e in enumerate(embeddings):
+        label = labels[i]
+        npe = np.array(e)
+        class_center_train[label] = class_center_train[label] + npe
+        class_size[label] += 1
+
+    return [e / class_size[i] for i, e in enumerate(class_center_train)]
+
+def print_stats_pure():
+    all_dists = []
+
+    for epoch in range(30):
+        print(epoch)
+        train_embeddings, train_labels, test_embeddings, test_labels = get_pure_data(epoch)
+        class_embeddings = get_class_centers(test_embeddings, test_labels, 10)
+
+        center = np.zeros(len(class_embeddings[0]))
+        for i in class_embeddings:
+            center += i
+        center = center / len(class_embeddings)
+        
+        dists = []
+
+        for i in class_embeddings:
+            dists.append(np.linalg.norm(center - i))
+
+        all_dists.append(dists)
+
+    plot.plotSurface([all_dists], "Distance To Class Center", [i for i in range(30)], "Epochs", [i for i in range(10)], "Classes", 1, [""])
+
+def get_pure_data(epoch) :
+    data = read_pickle_file(os.path.dirname(__file__) + f"/../embeddingData/classification_data_train_{epoch}.json")
+    train_embeddings = data["train_embeddings"]
     train_labels = data["train_labels"]
 
+    data = read_pickle_file(os.path.dirname(__file__) + f"/../embeddingData/classification_data_val_{epoch}.json")
+    test_embeddings = data["val_embeddings"]
+    test_labels = data["val_labels"]
+    return train_embeddings, train_labels, test_embeddings, test_labels
+
+def plot_pure():
+    best_epoch = 29
+    train_embeddings, train_labels, test_embeddings, test_labels = get_pure_data(best_epoch)
+
+    pca = PCA(n_components = 3)
+    train_projections = pca.fit_transform(train_embeddings)
+    test_projections  = pca.transform(test_embeddings)
+
+    COLOR = plot.get_colors(10)
+    series = [{"marker": ".", "color": COLOR[i % 10], "label": f"{i%10}", "points":[]} for i in range(10)]
+
+    class_embeddings = get_class_centers(test_embeddings, test_labels, 10)
+
+    misclassified = 0
+    for i, v in enumerate(test_embeddings):
+        label = test_labels[i]
+
+        clossest_class = argmin([((np.array(v) - np.array(c))**2).sum() for c in class_embeddings])
+        index = 0 if clossest_class == label else 1
+        misclassified += index
+        
+        series[label]["points"].append(train_projections[i])
+
+    print(f"{misclassified}/{len(test_embeddings)}, acc: {100.0 * (1.0 - misclassified/len(test_embeddings)):.2f}%")
+    plot.plotCustomPoints(series, legend=True, axes=[0,1,2])
+        
+def plot_pure_distance_distribution():
+    test_buckets = []
+    train_buckets = []
+    all_test_dists = []
+    all_train_dists = []
+    maximum = -float("inf")
+    minimum = float("inf")
+    for epoch in range(1, 30, 14):
+        print(epoch)
+        train_embeddings, train_labels, test_embeddings, test_labels = get_pure_data(epoch)
+        class_embeddings = get_class_centers(test_embeddings, test_labels, 10)
+
+        test_dists = list(filter(lambda x : x < 60, get_dists(test_embeddings, test_labels, class_embeddings, lambda x : x)))
+        train_dists = list(filter(lambda x : x < 60, get_dists(train_embeddings, train_labels, class_embeddings, lambda x : x)))
+
+        all_test_dists.append(test_dists)
+        all_train_dists.append(train_dists)
+        num_buckets = 100
+
+        maximum = max(max(test_dists), max(train_dists), maximum)
+        minimum = min(min(test_dists), min(train_dists), minimum)
+
+    for i in range(len(all_test_dists)):
+        test_buckets.append(get_buckets(all_test_dists[i], maximum, minimum, num_buckets))
+        train_buckets.append(get_buckets(all_train_dists[i], maximum, minimum, num_buckets))
+
+    plot.plot_line_2d([(i * (maximum - minimum) / (num_buckets) + minimum) for i in range(num_buckets)], 
+                      [test_buckets[0], test_buckets[1], test_buckets[2]], 
+                      ["Testing epoch 1", "Testing epoch 15", "Training epoch 29"], lambda x:x)
+
+    #plot.plotSurface([test_buckets, train_buckets], "Dist Frequency", [i for i in range(30)], "Epoch", 
+    #                 [(i * (maximum - minimum) / (num_buckets) + minimum) for i in range(num_buckets)], "Dist", 1, ["Test", "Train"])
+
+def plot_acc_by_epoch(): 
+    acc = []
+    for epoch in range(30):
+        misclassified = 0
+        
+        train_embeddings, train_labels, test_embeddings, test_labels = get_pure_data(epoch)
+        class_embeddings = get_class_centers(test_embeddings, test_labels, 10)
+
+        for i, v in enumerate(test_embeddings):
+            label = test_labels[i]
+
+            clossest_class = argmin([((np.array(v) - np.array(c))**2).sum() for c in class_embeddings])
+            misclassified += 0 if clossest_class == label else 1
+        
+        acc.append(100.0 * (1.0 - misclassified/len(test_embeddings)))
+    
+    plot.plot_simple_line_2d(acc, lambda x:x)
+
+def get_dists_by_label(embeddings, labels, class_embeddings):
+    dists = [[] for _ in range(10)]
+    for i, embedding in enumerate(embeddings):
+        class_embedding = class_embeddings[labels[i]]
+        dists[labels[i]].append(np.log(np.linalg.norm(np.array(class_embedding) - np.array(embedding))))
+    return dists
+
+def get_zipped(l, h):
+    output = []
+    for i, e in enumerate(l):
+        output.append(e + h[i])
+    return output
+
+def deep_filter(l, lvls, func, start_value):
+    if lvls == 0:
+        return l
+    else:
+        value = start_value
+        for v in l:
+            value = func(value, deep_filter(v, lvls - 1, func, start_value))
+        return value
+
+def plot_distance_distribution():
+    epoch = 0
+    train_embeddings, train_labels, test_embeddings, test_labels = get_pure_data(epoch)
+    class_embeddings = get_class_centers(test_embeddings, test_labels, 10)
+
+    test_dists = get_dists_by_label(test_embeddings, test_embeddings, class_embeddings)
+    train_dists = get_dists_by_label(train_embeddings, train_labels, class_embeddings)
+    all_dists = get_zipped(test_dists, train_dists)
+
+    num_buckets = 30
+
+    maximum = deep_filter(all_dists, 2, max, float("-inf"))
+    minimum = deep_filter(all_dists, 2, min, float("inf"))
+
+if __name__ == '__main__':
+    #data = read_json_file(os.path.dirname(__file__) + "/../../embeddingData/json_data.json")
+
+    #train_embeddings = data["train_embeddings"]
+    #test_embeddings  = data["test_embeddings"]
+    #class_embeddings = data["class_embeddings"]
+    #test_labels = data["test_labels"]
+    #train_labels = data["train_labels"]
+
+    print_stats_pure()
+    #plot_acc_by_epoch()
+    #plot_pure_distance_distribution()
 
     #print_distance_matrix(class_embeddings)
     #print_distances_from_center()
     #plot_series()
     #plot_pcas()
-    plot_distance_distribution()
+    #plot_distance_distribution()
     #plot_center_class_projection()
     print("Done")
-
-
-
-
-
-
-
-
-
-
 
