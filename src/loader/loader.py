56280e999faba44f0ef83afe9dd5076d54698080
StevenDankMeister <thedankpure@gmail.com>
2023-04-27 09:29:42 +0200

    Cifar100 fixed and added transforms

--- src/loader/loader.py
+++ src/loader/loader.py
@@ -63,6 +63,19 @@ transforms_dict = {
                         transforms.CenterCrop(36),
                         transforms.Normalize(mean=33.3149, std=78.5639)
                     ]),
+    "cifar10_resnet": transforms.Compose([
+                        # transforms.ToTensor(),
+                        transforms.Resize(44, antialias=True),
+                        transforms.CenterCrop(36),
+                        transforms.Normalize((0.491, 0.482, 0.447), (0.202, 0.200, 0.201))
+                    ]),
+
+    "cifar100_resnet": transforms.Compose([
+                    # transforms.ToTensor(),
+                    transforms.Resize(44, antialias=True),
+                    transforms.CenterCrop(36),
+                    transforms.Normalize((0.507, 0.487, 0.441), (0.201, 0.198, 0.202))
+                ]),
 }
 
 def load_data(train_data, test_data, batch_size=100):
@@ -305,7 +318,7 @@ def get_cifar10(config):
 
     train.targets = torch.tensor(train.targets, dtype=torch.int32)
     val.targets = torch.tensor(val.targets)
-
+    
     return train, val, testing_set
 
 def get_omniglot(config, target_alphabets=[]):
@@ -355,6 +368,12 @@ def get_cifar100(config):
         transform=transforms_dict[config.test_transforms],
         download=True
     ) 
+    
+    training_set.data = torch.from_numpy(training_set.data).permute(0,3,1,2).float()
+    testing_set.data = torch.from_numpy(testing_set.data).permute(0,3,1,2).float()
+    
+    # mean = train.data.mean(dim=(2,3)).mean(0)/255
+    # std = train.data.std(dim=(2,3)).mean(0)/255
     
     training_set.targets = torch.from_numpy(np.array(training_set.targets))
     testing_set.targets = torch.from_numpy(np.array(testing_set.targets))
