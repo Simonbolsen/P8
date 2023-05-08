74b22d936e7010b2920890262c40bc9832c54a67
Simon <Simonbolsen1234@gmail.com>
2023-05-08 15:33:35 +0200

    push and pull loss

--- src/nn_util.py
+++ src/nn_util.py
@@ -99,6 +99,25 @@ def simple_dist_loss(output_embds, class_embeds, targets, device):
     return acc_loss, None#, acc_loss_div
 
 
+def push_n_pull_loss(output_embds, class_embeds, targets, device):
+    acc_loss = torch.tensor(0.0, requires_grad=True, device=device)
+    avg_center = torch.sum(class_embeds, dim = 0) / class_embeds.size()[0]
+    
+    actual_embeds = class_embeds[targets]
+    class_embed_diffs = output_embds - actual_embeds
+    avg_center_diffs = output_embds - avg_center
+
+    ce_loss =  torch.sum(class_embed_diffs**2, dim = [0,1])
+    ac_loss = torch.sum(avg_center_diffs**2, dim = [0,1])
+    acc_loss = ce_loss - 0.5 * ac_loss
+
+    print(f"{ce_loss}, {ac_loss}")
+
+    return acc_loss, None
+
+def pnp_hyperparam(q):
+    return lambda output_embeds, class_embeds, targets, device : push_n_pull_loss(q, output_embeds, class_embeds, targets, device)
+
 def cone_loss(p, q, output_embeds, class_embeds, targets, device):
     acc_loss = torch.tensor(0.0, requires_grad=True, device=device)
     grad = torch.zeros(torch.Size([output_embeds.size()[0] + class_embeds.size()[0], output_embeds.size()[1]]), device=device, dtype=torch.float)
@@ -275,6 +294,7 @@ def one_hot_cone_loss(res, labels, device):
 
 emc_loss_functions = {
     "simple-dist": simple_dist_loss,
+    "pnp-loss": push_n_pull_loss,
     "class-push": dist_and_proximity_loss,
     "comp-dist-loss": comparison_dist_loss,
     "cone-loss": cone_loss_hyperparam,
@@ -291,6 +311,9 @@ def get_emc_loss_function(args, config):
     if args.loss_func == "cone-loss":
         logging.debug(f'cone loss: using values p: {config["p"]}, q: {config["q"]}')
         return loss_func(config["p"], config["q"])
+    if args.loss_func == "pnp-loss":
+        logging.debug(f"pnp loss: using value q: {config['q']}")
+        return loss_func(config["q"])
 
     return loss_func
 
