889bf621e8144858f118f8078995a1c65792f54a
Henrik Ginnerup <hginnerup@hotmail.com>
2023-05-15 10:05:11 +0200

    Group accuracy table by dataset

--- src/embeddingAnalysis/print_accuracies.py
+++ src/embeddingAnalysis/print_accuracies.py
@@ -1,15 +1,9 @@
 import json
 import os
-from ray import tune
-if __name__ == '__main__':
-    import results_analysis_util as analysis
-else:
-    import Plotting.results_analysis_util as analysis
+from analysis_util import get_exp_report_name
 
 
 def print_accuracies(plot_data):
-
-    name = plot_data["name"]
     def format_acc(acc): 
         return str(round(acc * 100, 1)) + "\%"
     
@@ -19,9 +13,10 @@ def print_accuracies(plot_data):
         if name in acc_per_loss_func:
             return format_acc(max(acc_per_loss_func[name]))
         else:
-            return "n/a"
+            return "-"
 
 
+    name       = get_exp_report_name(plot_data)
     cos_acc    = get_display_acc("cosine")
     cos_cd_acc = get_display_acc("cosine_ce")
     euc_acc    = get_display_acc("eucledian")
@@ -31,24 +26,39 @@ def print_accuracies(plot_data):
     print(name + " & " + cos_acc + " & " + cos_cd_acc + " & " + euc_acc + " & " + euc_ce_acc + " & " + pure_acc + " \\\\")
 
 
+def get_all_data(data_path):
+    data = dict()
+    for entry in os.scandir(path=data_path):
+        if entry.is_file():
+            with open(entry, "rb") as json_file:
+                data[entry.name] = json.loads(json.load(json_file))
+
+    return data
+
+def group_by(list, predicate):
+    groups = dict()
+    for x in list:
+        pred = predicate(x)
+        if not pred in groups:
+            groups[pred] = []
+
+        groups[pred].append(x)
+    return groups
+
 if __name__ == '__main__':
-    data_path = "./plots/plotData"
+    all_data = get_all_data("./plots/plotData")
+
+
+    data_per_dataset = group_by(all_data.values(), lambda x: x["meta_data"]["config"]["dataset"])
+
 
     print("\\begin{table}[]")
     print("\\begin{tabular}{lrrrr}")
     print("Experiment ID & cos_acc & cos_cd_acc & euc_acc & euc_ce_acc & pure\\\\")
-
-    for entry in os.scandir(path=data_path):
-        if entry.is_file():
-            entryname = entry.name
-            with open(entry, "rb") as json_file:
-                data = json.loads(json.load(json_file))
-
-                print_accuracies(data)
-                # try:
-                #     print_accuracies(data)
-                # except:
-                #     print("Failed to plot " + entryname)
+    for dataset_name, dataset in data_per_dataset.items():
+        print(dataset_name + " \\\\")
+        for data in dataset:
+            print_accuracies(data)
 
     print("\\end{tabular}")
     print("\\end{table}")
